c8a639f8e8dfe6994650d57cc90c49568c5c7bcc
ktamulonis <kurttamulonis@gmail.com>
2024-01-07 16:30:19 -0600

    add first changes AudioToVideo#audio_to_video method

---
+++ audio_to_video.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = "audio_to_video"
+  s.version     = "0.0.1"
+  s.summary     = "AudioToVideo#audio_to_video method lets you create videos easily"
+  s.description = "Convert an Audio and an Image into Videos easily!"
+  s.authors     = ["Enoch Tamulonis", "Kurt Tamulonis"]
+  s.email       = "wavclouds@gmail.com"
+  s.files       = ["lib/audio_to_video.rb"]
+  s.homepage    =
+    "https://rubygems.org/gems/audio_to_video"
+  s.license       = "MIT"
+end
---
+++ lib/audio_to_video.rb
@@ -0,0 +1,26 @@
+# Videos for your Songs
+class AudioToVideo
+  def self.audio_to_video(audio, image, &block)
+    # Supports Files or ActiveStorage::Attachment
+    image = conversion.image
+    loop_option = if image.blob.content_type.downcase.include?("gif")
+      "-ignore_loop 0" 
+    else
+      "-loop 1"
+    end
+
+    directory = 'tmp/audio_to_video'
+    # Create the directory if it doesn't exist
+    Dir.mkdir(directory) unless File.directory?(directory)
+
+    output_path = Rails.root.join("#{directory}/audio_#{Time.zone.now.strftime("%s_%m_video")}.mp4")
+
+    audio.open do |local_audio|
+      image.open do |local_image|
+        command = "ffmpeg #{loop_option} -i #{local_image.path} -i #{local_audio.path} -c:v libx264 -tune stillimage -c:a aac -strict experimental -b:a 192k -shortest #{movie_path}"
+        system(command)
+      end
+    end
+    output_path
+  end
+end
